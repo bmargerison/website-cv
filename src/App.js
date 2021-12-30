ee9dc9650da9f571ca94012024e101866cfda78f
bmargerison <bmargerison@outlook.com>
2021-12-30 14:34:51 +0000

    Set up react-router-dom and pags

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Navbar from "./components/Navbar.js"
-import About from "./components/About.js"
+import Intro from "./components/pages/Intro.js"
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
+    <>
     <main className="text-gray-400 bg-gray-900 body-font">
-      <Navbar />
-      <About />
+      <Router>
+        <Navbar />
+        <Routes>
+            <Route path='/' element={<Intro/>} />
+        </Routes>
+
+      </Router>
     </main>
+    </>
   );
 }
 
---
+++ src/components/pages/Intro.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import '../../App.js';
+import About from '../About.js';
+
+
+
+function Home() {
+  return (
+    <>
+      <About />
+    </>
+  );
+}
+
+export default Home;
