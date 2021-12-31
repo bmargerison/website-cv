8aaa91a8c884a62a8a9cdbfa8e1a6db05fc43e6e
bmargerison <bmargerison@outlook.com>
2021-12-31 12:50:08 +0000

    Skills section with placeholder text

--- src/components/pages/Portfolio.js
+++ src/components/pages/Portfolio.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../../App.js';
 import Projects from '../Projects.js';
+import Skills from '../Skills.js';
 
 function Portfolio() {
   return (
     <>
+      <Skills />  
       <Projects />
     </>
   );
